ab76226012a4ee9546a0267c890529e5d17d95a0
Emi Brown <emibrown55@gmail.com>
2017-07-29 13:38:14 -0500

    13:38
    Added menu class - no image yet

---
+++ src/main/Menu.java
@@ -0,0 +1,21 @@
+package main;
+
+import graphics.Screen;
+import graphics.Texture;
+
+public class Menu {
+	public void render(Screen screen) {
+		screen.drawTexture(0, 0, Menubackground);
+	}
+	
+	public void update() {
+		
+	}
+	
+	public Menu() {
+		Menubackground = new Texture("name", "/sprites/___.png", 1960, 540);
+	}
+	
+	Texture Menubackground;
+			
+}
