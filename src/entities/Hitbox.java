b36cad419f4ca83f1646f86c23a536107b5e11b5
firstdreamding <huaxu3721@yahoo.com>
2017-07-29 13:55:59 -0500

    m

--- src/entities/Hitbox.java
+++ src/entities/Hitbox.java
@@ -3,18 +3,23 @@ package entities;
 import java.awt.Rectangle;
 
 public class Hitbox extends Rectangle {
+	int m = 1;
 	public Hitbox(String x, String y, String w, String h) {
-		this.x = Integer.parseInt(x);
-		this.y = Integer.parseInt(y);
+		this.x = Integer.parseInt(x) * m;
+		this.y = Integer.parseInt(y) * m;
 		width = Integer.parseInt(w);
 		height = Integer.parseInt(h);
 	}
 
 	public Hitbox(int x, int y, int w, int h) {
-		this.x = x;
-		this.y = y;
+		this.x = x * m;
+		this.y = y * m;
 		width = w;
 		height = h;
+	}
+	
+	public void changeM(int x){
+		m = x;
 	}
 
 	public void shiftX(int i) {
