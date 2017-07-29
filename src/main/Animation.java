ab04f45a526223dfb4518d83a041de6ba4130e55
firstdreamding <huaxu3721@yahoo.com>
2017-07-29 12:32:33 -0500

    12 32

--- src/main/Animation.java
+++ src/main/Animation.java
@@ -1,14 +1,21 @@
 package main;
 
+import java.lang.reflect.Method;
+
 public class Animation {
 	 
 	String animationPlaying;
 	
-	public void update() { //this method tries to update every 60 frames
+	public void update() throws Throwable { //this method tries to update every 60 frames
+		
+		Method method = getClass().getDeclaredMethod(animationPlaying);
+		method.invoke(this);
 		
 	}
 	
-	
+	public void walking(){
+		
+	}
 	
 	
 }
