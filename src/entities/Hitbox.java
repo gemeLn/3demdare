196799ee6066b5fe9ebee67c0a51313d4da33a5e
gemeLn <mtang317@gmail.com>
2017-07-28 23:53:09 -0500

    11:53

--- src/entities/Hitbox.java
+++ src/entities/Hitbox.java
@@ -3,7 +3,9 @@ package entities;
 import java.awt.Rectangle;
 
 public class Hitbox extends Rectangle {
-	public Hitbox(int w, int h) {
+	public Hitbox(int x, int y, int w, int h) {
+		this.x = x;
+		this.y = y;
 		width = w;
 		height = h;
 	}
