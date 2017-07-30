2ad3601ff127b82d2dd93f59e3b70e8bd1437b02
Emi Brown <emibrown55@gmail.com>
2017-07-30 15:27:05 -0500

    3:27

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -7,6 +7,7 @@ import graphics.Texture;
 
 public class Menu {
 	Texture menuBackground;
+	int currentbackground;
 	ArrayList<Texture> menuHighlight = new ArrayList<Texture>();
 
 	public void render(Screen screen) {
@@ -19,25 +20,40 @@ public class Menu {
 	}
 
 	public Menu() {
+		currentbackground = 0;
+		
 		menuBackground = new Texture("Menu Background", "/sprites/menubackground.png", 960, 540);
 		menuHighlight.add(new Texture("Menu Play", "/sprites/menubackgroundPlay.png", 960, 540));
-
+		menuHighlight.add(new Texture("Menu Safareee", "/sprites/menubackgroundPlay.png", 960, 540));
+		menuHighlight.add(new Texture("Menu AppStore", "/sprites/menubackgroundPlay.png", 960, 540));
+		menuHighlight.add(new Texture("Menu Contacts", "/sprites/menubackgroundPlay.png", 960, 540));
+		menuHighlight.add(new Texture("Menu Settings", "/sprites/menubackgroundPlay.png", 960, 540));
 	}
 
 	public void downPressed() { // active when u press down key
-
+		currentbackground = 1;
+		menuBackground = menuHighlight.get(currentbackground);
 	}
 
 	public void upPressed() { // active when u press up key
-		menuBackground = menuHighlight.get(0);
-		System.out.println("hi");
+		currentbackground = 0;
+		menuBackground = menuHighlight.get(currentbackground);
+		//System.out.println("hi");
 	}
 
 	public void leftPressed() { // etc...
-
+		if (menuBackground > 0) {
+			currentbackground --;
+		}
+		
+		menuBackground = menuHighlight.get(currentbackground);
 	}
 
 	public void rightPressed() {
-
+		if (menuBackground > 0) {
+			currentbackground ++;
+		}
+		
+		menuBackground = menuHighlight.get(currentbackground);
 	}
 }
