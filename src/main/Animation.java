6d2760a9b2b9ed3caabf06f8e245b9df99efdd3c
firstdreamding <huaxu3721@yahoo.com>
2017-07-30 19:35:13 -0500

    7 35

--- src/main/Animation.java
+++ src/main/Animation.java
@@ -2,9 +2,18 @@ package main;
 
 import java.lang.reflect.Method;
 
+import graphics.SpriteSheet;
+import graphics.Texture;
+
 public class Animation {
 	 
 	String animationPlaying;
+	SpriteSheet player;
+	int index = 5;
+	
+	public Animation(){
+		//player = new SpriteSheet(new Texture("/sprites/walkcyclenoborder.png",1150, 50), 50, 50);
+	}
 	
 	public void update() throws Throwable { //this method tries to update every 60 frames
 		
@@ -13,8 +22,10 @@ public class Animation {
 		
 	}
 	
-	public void walking(){
-		
+	public Texture walking(){
+		if(index < 22)
+			index++;
+		return player.getTexture(index, 1);
 	}
 	
 	public void jumping(){
